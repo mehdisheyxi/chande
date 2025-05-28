edb1108028cf008147be8dce85fd216cbfe9482b
mehdi <mehdisheyxi@gmail.com>
2025-05-27 20:09:13 -0400

    not ok

--- Core.py
+++ Core.py
@@ -5,27 +5,40 @@ import fake_useragent
 
 
 #create random user_agent for website
-ua = fake_useragent.UserAgent()
-headers = {'user-agent': ua.random}
-
 url = 'https://www.tgju.org/'
-response = requests.get(url, headers=headers)
 
-soap = BeautifulSoup(response.text,'html.parser')
-price = soap.find('td',class_='market-price')
+def get_prices(url):
+    class_name = [
+        'market-price',
+        'high',
+        'market-low',
+        'market-high',
+        'market-time'
+    ]
+    ua = fake_useragent.UserAgent()
+    headers = {'user-agent': ua.random}
+    response = requests.get(url, headers=headers)
+    soap = BeautifulSoup(response.text,'html.parser')
+    price = soap.find_all('td',attrs={'class':class_name})
+    return price
+    # if price:
+    #     raw_price = price.text.strip()  # حذف فضای خالی اول و آخر
+    #     clean_price = raw_price.replace(',', '')
+    #
+    #     try:
+    #         price_int = int(clean_price)
+    #         return (f'this is final OK:{price_int:,}\n xobe ??!!')
+    #     except ValueError:
+    #         return (f'NOT OK:{clean_price}')
+    # else:
+    #     return ('NOT FOUND')
 
 
-if price:
-    raw_price = price.text.strip()  # حذف فضای خالی اول و آخر
-    clean_price = raw_price.replace(',', '')
-
-    try:
-        price_int = int(clean_price)
-        print(f'this is final OK:{price_int:,}\n xobe ??!!')
-    except ValueError:
-        print(f'NOT OK:{clean_price}')
-else:
-    print('NOT FOUND')
 
 
 
+
+
+
+
+print(get_prices(url))
