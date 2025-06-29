5f8125d0efdce558af46ba8cf7dddfd1dd9890da
mehdisheyxi <mehdisheyxi@gmail.com>
2025-06-30 00:53:22 +0330

    update icon

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import sys
+
+from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtWidgets import QMainWindow
 from PyQt5.uic import loadUi  # لود کردن فایل .ui
@@ -6,7 +8,9 @@ from PyQt5.uic import loadUi  # لود کردن فایل .ui
 class MyApp(QMainWindow):
     def __init__(self):
         super().__init__()
-        loadUi("UI/main_window.ui", self)  # نام فایل .ui را تنظیم کن
+        loadUi("UI/main_window2.ui", self)  # نام فایل .ui را تنظیم کن
+        self.setWindowTitle("chande")
+        self.setWindowIcon(QIcon("logo/logochande.png"))
 
 
 if __name__ == "__main__":
